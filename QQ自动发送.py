103a5bda343e2a2aea8aef5dae1a847961bfc5e1
xiaosi4081 <62277361+xiaosi4081@users.noreply.github.com>
2021-04-27 18:59:29 +0800

    Update QQ自动发送.py

--- QQ自动发送.py
+++ QQ自动发送.py
@@ -33,3 +33,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+版权声明：本文为CSDN博主「时暑」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/wjb123sw99/article/details/83475516
+
